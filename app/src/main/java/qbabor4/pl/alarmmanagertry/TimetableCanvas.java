e1c26e886e57a4543205cecd54f71676e3306dae
jakub <qbabor4444@gmail.com>
2017-12-11 21:16:11 +0100

    working on adding rectangles to timetable

--- app/src/main/java/qbabor4/pl/alarmmanagertry/TimetableCanvas.java
+++ app/src/main/java/qbabor4/pl/alarmmanagertry/TimetableCanvas.java
@@ -49,9 +49,10 @@ public class TimetableCanvas extends AppCompatActivity implements SurfaceHolder.
     private int scheduleHeight;
     private Canvas canvas;
 
-    private static final int TIME_SECTION_SIZE = 120;
+    private static final int TIME_SECTION_SIZE = 150;
     private static final int DAYS_SECTION_SIZE = 80;
     private static final int NO_LINE_SIZE = 10;
+    private static final int RECTAGLE_HORISONTAL_PADDING = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -141,10 +142,9 @@ public class TimetableCanvas extends AppCompatActivity implements SurfaceHolder.
         // robić na width i height bez
 
         // ustawiac rectangle procentowo w zależnosci od długości zajęć
-        makeRectange(300, 200, 500, 400, "#f48342");
+//        makeRectange(300, 200, 500, 400, "#f48342");
 //        drawText("lol");
 
-//        makeRectange(370, 50, 500, 300,  "#41f474");
 
         Toast.makeText(TimetableCanvas.getInstance(), rectangleClasses.toString(), Toast.LENGTH_SHORT).show();
 
@@ -159,12 +159,12 @@ public class TimetableCanvas extends AppCompatActivity implements SurfaceHolder.
         paint.setStyle(Paint.Style.FILL);
         paint.setColor(Color.parseColor("#aaaaaa"));
         canvas.drawLine(NO_LINE_SIZE, DAYS_SECTION_SIZE, canvasSurfaceViewWidth - NO_LINE_SIZE, DAYS_SECTION_SIZE, paint);
-        canvas.drawLine(TIME_SECTION_SIZE, NO_LINE_SIZE, TIME_SECTION_SIZE, canvasSurfaceViewHeight - NO_LINE_SIZE, paint);
+//        canvas.drawLine(TIME_SECTION_SIZE, NO_LINE_SIZE, TIME_SECTION_SIZE, canvasSurfaceViewHeight - NO_LINE_SIZE, paint);
 
         // srodkowanie w pionie nie nie podstawie bounds tylko bez ogonków i kresek robić jakoś
 
 
-        String[] daysOfWeek = {"Pon", "Wt", "Śr", "Czw", "Pt", "Sob"}; // pobierane na podstawie bazy i języka (pol/ang)
+        String[] daysOfWeek = {"Pon", "Wt", "Śr", "Czw", "Pt", "Sob", "Ndz"}; // pobierane na podstawie bazy i języka (pol/ang)
 
         int rectangleWidth = scheduleWidth / daysOfWeek.length; // bo 5 dni tygodnia (jak bedie wiecej, to zmienic na tyle ile jest dni
 
@@ -175,10 +175,75 @@ public class TimetableCanvas extends AppCompatActivity implements SurfaceHolder.
             paintText.setTextSize(50);
             Rect bounds = new Rect();
             paintText.getTextBounds(day, 0, day.length(), bounds);
-            canvas.drawText(day, TIME_SECTION_SIZE + (i*2+1)*((scheduleWidth / (daysOfWeek.length*2))) - bounds.width() / 2, DAYS_SECTION_SIZE / 2 + bounds.height() / 2, paintText); //  /10 bo jest 5 dni /5 /2
+            canvas.drawText(day, TIME_SECTION_SIZE + (i * 2 + 1) * ((scheduleWidth / (daysOfWeek.length * 2))) - bounds.width() / 2, DAYS_SECTION_SIZE / 2 + bounds.height() / 2, paintText); //  /10 bo jest 5 dni /5 /2
             // 10
-            Log.d("lol", String.valueOf(TIME_SECTION_SIZE + (scheduleWidth / (daysOfWeek.length*2)) - bounds.width() / 2));
+            Log.d("lol", String.valueOf(TIME_SECTION_SIZE + (scheduleWidth / (daysOfWeek.length * 2)) - bounds.width() / 2));
         }
+
+        // linie ze środka godzin
+
+        // ustawić godziny tak, żeby pokazywało jedną więcej albo co tyle co by łoło jedną mniej
+
+        // patrzec na pierwszą godzinę i ostatnią i tak pokazać (pokazać co pół godziny
+
+        // w zaleznosci od czaso pomiedzy startem i koncem wyswietlac co 15 min, 30min albo co godzinę
+
+        int minTime = 8 * 60;
+        int maxTime = 16 * 60; // w minutach // t
+        int timeDifference = maxTime - minTime;
+
+        int numOfTimesDisplayed = 16 ;
+
+        for (int i = 0; i < numOfTimesDisplayed+1; i++) {
+            Log.d("minuty", String.valueOf(minTime + (timeDifference / numOfTimesDisplayed) * i));
+            int minutesTotal = minTime + (timeDifference / numOfTimesDisplayed) * i;
+
+            int hours = minutesTotal / 60;
+            String minutes = String.valueOf(minutesTotal % 60);
+//            Log.d("minuty", String.valueOf(minutes) );
+//            Log.d("ho", String.valueOf(hours) );
+            Paint paintText = new Paint();
+            paintText.setColor(Color.BLACK);
+            paintText.setTextSize(50);
+            // jak długosc minut jest 1 to dodac 0 z przodu
+            if (minutes.length() == 1){
+                minutes = "0" + minutes;
+            }
+            String timeToDraw = hours + ":" + minutes;
+            Rect bounds = new Rect();
+            paintText.getTextBounds(timeToDraw, 0, timeToDraw.length(), bounds);
+            canvas.drawLine(TIME_SECTION_SIZE , DAYS_SECTION_SIZE + (i * 2 + 1) * ((scheduleHeight / ((numOfTimesDisplayed+1) * 2))), canvasSurfaceViewWidth, DAYS_SECTION_SIZE + (i * 2 + 1) * ((scheduleHeight / ((numOfTimesDisplayed+1) * 2))), paintText);
+            canvas.drawText(timeToDraw, TIME_SECTION_SIZE /2 - bounds.width()/2 , DAYS_SECTION_SIZE + (i * 2 + 1) * ((scheduleHeight / ((numOfTimesDisplayed+1) * 2))) + bounds.height() / 2, paintText);
+        }
+
+        // narysowac prostokąt podając godziny i numer dnia (dzien)
+        int i = 0;  // 8:00
+        int y1 = DAYS_SECTION_SIZE + (i * 2 + 1) * ((scheduleHeight / ((numOfTimesDisplayed+1) * 2)));
+        int j = 16; // 10:00
+        int y2 = DAYS_SECTION_SIZE + (j * 2 + 1) * ((scheduleHeight / ((numOfTimesDisplayed+1) * 2)));
+        int k = 1;
+        int x1 = TIME_SECTION_SIZE + (scheduleWidth / (daysOfWeek.length)) * k + RECTAGLE_HORISONTAL_PADDING;
+        int l = 2;
+        int x2 = TIME_SECTION_SIZE + (scheduleWidth / (daysOfWeek.length)) * l - RECTAGLE_HORISONTAL_PADDING;
+//        canvas.drawRect(new Rect(x1, y1, x2, y2), paint);
+        makeRectange(x1,y1,x2,y2,"#f48342");
+
+        // dostac z dni x1 x2
+    }
+
+    private void addRectangleClass(int startTime, int stopTime, int dayOfWeek){ // jeszcze jakiś text w środku i kolor rectangla i textu
+        // zakładam, że zawsze bedzie 17 czasów pokazanych
+        // zakładam, że podają co min 30 min (potem sie zmieni)
+        // dstac y pierwszej lini i ostatniej a potem dzielic biorąc czas pomiędzy wszystkimi a czas trwania lekcji
+        int numOfTimesDisplayed = 16;
+        int i = 0;  // 8:00
+        int y1 = DAYS_SECTION_SIZE + (i * 2 + 1) * ((scheduleHeight / ((numOfTimesDisplayed+1) * 2)));
+        int j = numOfTimesDisplayed; // 16:00
+        int y2 = DAYS_SECTION_SIZE + (j * 2 + 1) * ((scheduleHeight / ((numOfTimesDisplayed+1) * 2)));
+
+        int spaceBetweenStartAndEnd = y2 - y1; // ogólna wysokosc od 8 do 16
+        // dodawac y1 bo od tego ma iść
+        // TODO
     }
 
     private void setCanvas(SurfaceHolder holder) {
