03ca707c2f5ebcaa6513619e7eeef1673e860ae6
jakub <qbabor4444@gmail.com>
2017-12-13 20:04:00 +0100

    cleans in code and new ideas for methods

---
+++ app/src/main/java/qbabor4/pl/alarmmanagertry/SqlDataEnum.java
@@ -0,0 +1,11 @@
+package qbabor4.pl.alarmmanagertry;
+
+/**
+ * Created by Jakub on 13-Dec-17.
+ */
+
+public enum SqlDataEnum {
+    DAY,
+    START_TIME,
+    STOP_TIME;
+}
--- app/src/main/java/qbabor4/pl/alarmmanagertry/TimetableCanvas.java
+++ app/src/main/java/qbabor4/pl/alarmmanagertry/TimetableCanvas.java
@@ -21,17 +21,16 @@ import android.view.ViewTreeObserver;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 /**
  * TODO
- * clickable rectangle
- * dostac wymiery layoutu
- * zrobić siatkę z dniami
  * wybieranie koloru w dodawaniu zajęć albo same sie beda zmieniac
  * klawisze prawo i lewo zeby zobaczy kolojne tygodnie
- * pokazywanie siatki godzinowej na podstawie minialnej godziny i maksymalnej w tygodniu
+ * <p>
  * TODO IFTIME:
  * zmiana nagólwka z dniami i czasami dynamicznie (robienie wedłód procentów
  * <p>
@@ -49,10 +48,22 @@ public class TimetableCanvas extends AppCompatActivity implements SurfaceHolder.
     private int scheduleHeight;
     private Canvas canvas;
 
-    private static final int TIME_SECTION_SIZE = 150;
+    private int startTimeDisplayed;
+    private int stopTimeDisplayed;
+    private int numOfTimesDisplayed;
+    private int gapBetweenTimesDisplayed;
+    private int firstLineYValue; // moze to lokalnie w drawDefault? albo drawRectangleClasses
+    private int lastLineYValue; // ^
+    private String[] daysOfWeek;
+
+    private static final int TIME_SECTION_SIZE = 150; // moze to sie zmieni na procenty v v v
     private static final int DAYS_SECTION_SIZE = 80;
-    private static final int NO_LINE_SIZE = 10;
+    private static final int NO_LINE_SIZE = 15;
     private static final int RECTAGLE_HORISONTAL_PADDING = 10;
+
+    private int lol(){
+        return 5;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,7 +72,6 @@ public class TimetableCanvas extends AppCompatActivity implements SurfaceHolder.
         setInstance();
         setToolbar();
         setSurfaceView();
-
     }
 
     private void setSurfaceView() {
@@ -75,6 +85,7 @@ public class TimetableCanvas extends AppCompatActivity implements SurfaceHolder.
                 canvasSurfaceViewHeight = canvasSurfaceView.getHeight();
                 canvasSurfaceViewWidth = canvasSurfaceView.getWidth();
                 setScheduleSize();
+                Log.d("surlol", "lol");
                 canvasSurfaceView.getViewTreeObserver().removeGlobalOnLayoutListener(this);
             }
         });
@@ -132,32 +143,16 @@ public class TimetableCanvas extends AppCompatActivity implements SurfaceHolder.
     public void surfaceCreated(SurfaceHolder holder) {
         // tu bedzie pobierał z bazy i na tej podstawie rysował rectangle
         rectangleClasses = new ArrayList<>();
+
         setCanvas(holder);
-
         drawDefault();
-//        Log.d("in", String.valueOf(canvasSurfaceViewHeight));
-
-        // zrobić siatkę z godzinami i dniami na podstawie szerokości canvasa
-
-        // robić na width i height bez
-
-        // ustawiac rectangle procentowo w zależnosci od długości zajęć
-//        makeRectange(300, 200, 500, 400, "#f48342");
-//        drawText("lol");
-
+        drawRectangleClasses();
 
         Toast.makeText(TimetableCanvas.getInstance(), rectangleClasses.toString(), Toast.LENGTH_SHORT).show();
 
         canvasTouchListener.setRectangles(rectangleClasses);
         holder.unlockCanvasAndPost(canvas);
     }
-
-    private int startTimeDisplayed;
-    private int stopTimeDisplayed;
-    private int numOfTimesDisplayed;
-    private int gapBetweenTimesDisplayed;
-    private int firstLineYValue;
-    private int lastLineYValue;
 
     private void drawDefault() {
 
@@ -166,145 +161,170 @@ public class TimetableCanvas extends AppCompatActivity implements SurfaceHolder.
         // jak nic nie ma w bazie, to wyswietlic od 8 do 16 plan
         // co któraś linia w innym kolorze? moze kazda inna co 3 w innym kolorze (odcienie szarego)
 
-        setGlobalGapAndStartAndStopTimeDisplayed(540, 960); // 9 do 16
-        setGlobalValuesOfFirstAndLastLineOnYaxis();
+        setGlobalGapAndStartAndStopTimeDisplayed(540, 960); // 9 do 16 // pierwsza i ostatnia godzina w planie
+        setGlobalValuesOfFirstAndLastLineOnYaxis(); // to jakos inaczej, bez globalnych jak sie da
 
-//        Log.d("num1", "" + ""+ this.startTimeDisplayed);
-//        Log.d("num2", "" + ""+ this.stopTimeDisplayed);
-//        Log.d("num3", "" + ""+ this.numOfTimesDisplayed);
-//        Log.d("num4", "" + ""+ this.gapBetweenTimesDisplayed);
+        drawLineUnderDays();
 
+        daysOfWeek = getDays(0); // pobierane na podstawie bazy i języka (pol/ang) (brać kawałek jak sie dowiem z bazy
+
+        drawDaysNames(daysOfWeek); // daysOfWeek chyba beda globalnie, albo tylko długość arrraya
+        drawTimesWithLines();
+    }
+
+    private String[] getDays(int lastDay) { //TODO
+        return new String[]{"Pon", "Wt", "Śr", "Czw", "Pt"};
+    }
+
+    private void drawRectangleClasses() { // podawac liste z mapami
+        // testowo
+        // 10:00 - 12:00 we wtorek
+        Map<SqlDataEnum, String> classData = new HashMap<>();
+        classData.put(SqlDataEnum.DAY, "1");
+        classData.put(SqlDataEnum.START_TIME, "600");
+        classData.put(SqlDataEnum.STOP_TIME, "720");
+        drawRectangle(classData);
+
+    }
+
+    private int getRowWidth() {
+        return scheduleWidth / daysOfWeek.length;
+    }
+
+    private int getXofScheduleEnd() {
+        return TIME_SECTION_SIZE + scheduleWidth;
+    }
+
+    private int getXofScheduleStart() {
+        return TIME_SECTION_SIZE;
+    }
+
+    private void drawRectangle(Map<SqlDataEnum, String> classData) {
+        int day = Integer.parseInt(classData.get(SqlDataEnum.DAY)); // numer dnia (1)
+        int startX = getXofScheduleStart(); // globalne finalne
+        int endX = getXofScheduleEnd();
+        int rowWidth = getRowWidth();
+
+        // wyznaczyć x1 i x2 gdzie sie ma pokazac
+    }
+
+    private void getXofRectangleStart(){
+
+    }
+
+
+    private void drawRectangle(int left, int top, int right, int bottom, String hexColor) {
+        Rect rect = new Rect(left, top, right, bottom);
+        rectangleClasses.add(rect);
         Paint paint = new Paint();
         paint.setStyle(Paint.Style.FILL);
-        paint.setColor(Color.parseColor("#aaaaaa"));
-        canvas.drawLine(NO_LINE_SIZE, DAYS_SECTION_SIZE, canvasSurfaceViewWidth - NO_LINE_SIZE, DAYS_SECTION_SIZE, paint); // horisontal line under days of week
+        paint.setColor(Color.parseColor(hexColor));
+        canvas.drawRect(rect, paint);
+    }
 
-        // srodkowanie w pionie nie nie podstawie bounds tylko bez ogonków i kresek robić jakoś
-
-
-        String[] daysOfWeek = {"Pon", "Wt", "Śr", "Czw", "Pt", "Sob", "Ndz"}; // pobierane na podstawie bazy i języka (pol/ang) (brać kawałek jak sie dowiem z bazy
-
-        int rectangleWidth = scheduleWidth / daysOfWeek.length; // bo 5 dni tygodnia (jak bedie wiecej, to zmienic na tyle ile jest dni
-
-        for (int i = 0; i < daysOfWeek.length; i++) {
-            String day = daysOfWeek[i];
-            Paint paintText = new Paint();
-            paintText.setColor(Color.BLACK);
-            paintText.setTextSize(50);
-            Rect bounds = new Rect();
-            paintText.getTextBounds(day, 0, day.length(), bounds);
-            canvas.drawText(day, TIME_SECTION_SIZE + (i * 2 + 1) * ((scheduleWidth / (daysOfWeek.length * 2))) - bounds.width() / 2, DAYS_SECTION_SIZE / 2 + bounds.height() / 2, paintText); //  /10 bo jest 5 dni /5 /2
-            // 10
-            Log.d("lol", String.valueOf(TIME_SECTION_SIZE + (scheduleWidth / (daysOfWeek.length * 2)) - bounds.width() / 2));
-        }
-
-//        int timeDifference = stopTimeDisplayed - startTimeDisplayed;
-
-        for (int i = 0; i < numOfTimesDisplayed  ; i++) { // 17 czasów (moze dac do zmiennej ile wyświetlać?
-
-
-
-            Paint paintOfTime = getPaintOfTimeToDisplay();
+    private void drawTimesWithLines() {
+        Paint paintOfTime = getPaintOfTimeToDisplay();
+        for (int i = 0; i < numOfTimesDisplayed; i++) { // 17 czasów (moze dac do zmiennej ile wyświetlać)
             String timeToDraw = getTimeToDisplay(i);
             Rect bounds = new Rect();
             paintOfTime.getTextBounds(timeToDraw, 0, timeToDraw.length(), bounds);
-
-            // inaczej dostac te texty, zeby ostawiało w srodku bez ogonków
-
-            canvas.drawLine(TIME_SECTION_SIZE,  getYaxisOfLine(i), canvasSurfaceViewWidth,  getYaxisOfLine(i), paintOfTime );
-            canvas.drawText(timeToDraw, getXaxisOfTimeDisplayed(bounds), getYaxisOfTimeDisplayed(i, bounds), paintOfTime );
+            canvas.drawLine(TIME_SECTION_SIZE, getYaxisOfLine(i), canvasSurfaceViewWidth, getYaxisOfLine(i), paintOfTime);
+            canvas.drawText(timeToDraw, getXaxisOfTimeDisplayed(bounds), getYaxisOfTimeDisplayed(i, bounds), paintOfTime);
         }
 
-        // narysowac prostokąt podając godziny i numer dnia (dzien)
-//        int i = 0;  // 8:00
-//        int y1 = DAYS_SECTION_SIZE + (i * 2 + 1) * ((scheduleHeight / ((numOfTimesDisplayed + 1) * 2))); // bez 1 ?
-//        int j = 16; // 16:00
-//        int y2 = DAYS_SECTION_SIZE + (j * 2 + 1) * ((scheduleHeight / ((numOfTimesDisplayed + 1) * 2)));
-//        int k = 1;
-//        int x1 = TIME_SECTION_SIZE + (scheduleWidth / (daysOfWeek.length)) * k + RECTAGLE_HORISONTAL_PADDING;
-//        int l = 2;
-//        int x2 = TIME_SECTION_SIZE + (scheduleWidth / (daysOfWeek.length)) * l - RECTAGLE_HORISONTAL_PADDING;
-//        makeRectange(x1, y1, x2, y2, "#f48342");
     }
 
-    private Paint getPaintOfTimeToDisplay(){
+    private void drawDaysNames(String[] daysOfWeek) {
+        Paint paintDay = getPaintOfDaysToDisplay();
+        for (int i = 0; i < daysOfWeek.length; i++) {
+
+            String day = daysOfWeek[i];
+            Rect bounds = new Rect();
+            paintDay.getTextBounds(day, 0, day.length(), bounds);
+
+            canvas.drawText(day, TIME_SECTION_SIZE + (i * 2 + 1) * ((scheduleWidth / (daysOfWeek.length * 2))) - bounds.width() / 2, DAYS_SECTION_SIZE / 2 + paintDay.getTextSize() / 4, paintDay); // getTextSize is /4 becouse it looks better, and positions ok
+        }
+    }
+
+    private void drawLineUnderDays() {
+        Paint paintOfLineUnderDays = getPaintOfLineUnderDays();
+        canvas.drawLine(NO_LINE_SIZE, DAYS_SECTION_SIZE, canvasSurfaceViewWidth - NO_LINE_SIZE, DAYS_SECTION_SIZE, paintOfLineUnderDays); // horisontal line under days of week
+    }
+
+    private Paint getPaintOfLineUnderDays() {
+        Paint paint = new Paint();
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.parseColor("#aaaaaa"));
+        return paint;
+    }
+
+    private Paint getPaintOfDaysToDisplay() {
+        Paint paintDay = new Paint();
+        paintDay.setColor(Color.BLACK);
+        paintDay.setTextSize(50);
+        return paintDay;
+    }
+
+    private Paint getPaintOfTimeToDisplay() {
         Paint paintText = new Paint();
         paintText.setColor(Color.BLACK);
         paintText.setTextSize(50);
         return paintText;
     }
 
-    private String getTimeToDisplay(int index){
+    private String getTimeToDisplay(int index) {
         int minutesTotal = startTimeDisplayed + gapBetweenTimesDisplayed * index;
         int hours = minutesTotal / 60;
         String minutes = String.valueOf(minutesTotal % 60);
-        if (minutes.length() == 1){
+        if (minutes.length() == 1) {
             minutes = '0' + minutes;
         }
         return hours + ":" + minutes;
     }
 
-    private int getXaxisOfTimeDisplayed(Rect bounds){
+    private int getXaxisOfTimeDisplayed(Rect bounds) {
         return TIME_SECTION_SIZE / 2 - bounds.width() / 2;
     }
 
-    private int getYaxisOfTimeDisplayed(int index, Rect bounds){
+    private int getYaxisOfTimeDisplayed(int index, Rect bounds) {
         return getYaxisOfLine(index) + bounds.height() / 2;
     }
 
-    private int getYaxisOfLine(int indexOfLinOnScreen){
-       return DAYS_SECTION_SIZE + (indexOfLinOnScreen * 2 + 1) * ((scheduleHeight / ((numOfTimesDisplayed ) * 2))); // czy ja bede potrzebowal w innych miejscach numOfTimesDisplayed? (moze podawac do funkcji
+    private int getYaxisOfLine(int indexOfLinOnScreen) {
+        return DAYS_SECTION_SIZE + (indexOfLinOnScreen * 2 + 1) * ((scheduleHeight / ((numOfTimesDisplayed) * 2))); // czy ja bede potrzebowal w innych miejscach numOfTimesDisplayed? (moze podawac do funkcji
     }
 
     private void setGlobalValuesOfFirstAndLastLineOnYaxis() {
         firstLineYValue = getYaxisOfLine(0);
-        lastLineYValue = getYaxisOfLine(numOfTimesDisplayed -1);
+        lastLineYValue = getYaxisOfLine(numOfTimesDisplayed - 1);
     }
 
-
-    private void setGlobalGapAndStartAndStopTimeDisplayed(int startTime, int stopTime){
+    private void setGlobalGapAndStartAndStopTimeDisplayed(int startTime, int stopTime) {
         setGapAndStartAndStopTimeDisplayed(startTime, stopTime, 15); // starts looking with 15 min gap
     }
 
-
-    private void setGapAndStartAndStopTimeDisplayed(int startTime, int stopTime, int gap){ // setuje czas poczatkowy i koncowy
+    private void setGapAndStartAndStopTimeDisplayed(int startTime, int stopTime, int gap) { // setuje czas poczatkowy i koncowy
         startTimeDisplayed = startTime - startTime % gap;
-        int stopTimeCounted = stopTime ; // jak bedzie 8:07 to doda 8 i bedzie 8 :15
-        if (stopTime%gap != 0) {
+        int stopTimeCounted = stopTime; // jak bedzie 8:07 to doda 8 i bedzie 8 :15
+        if (stopTime % gap != 0) {
             stopTimeCounted += gap - stopTime % gap;
         }
-        numOfTimesDisplayed = (stopTimeCounted - startTimeDisplayed)/gap +1;
+        numOfTimesDisplayed = (stopTimeCounted - startTimeDisplayed) / gap + 1;
         gapBetweenTimesDisplayed = gap;
 
-        if (numOfTimesDisplayed > 17){                                                 /// mozna podawac ile ma sie wyswietlać
-            setGapAndStartAndStopTimeDisplayed(startTime, stopTime, gap *2);
+        if (numOfTimesDisplayed > 17) {                                                 /// mozna podawac ile ma sie wyswietlać
+            setGapAndStartAndStopTimeDisplayed(startTime, stopTime, gap * 2);
         } else {
             stopTimeDisplayed = startTimeDisplayed + 16 * gap;  // 17 -1
             numOfTimesDisplayed = 17;
-            if (stopTimeDisplayed > 1440){ // jak wiecej od 24
+            if (stopTimeDisplayed > 1440) { // jak wiecej od 24
                 stopTimeDisplayed = 1440;
                 Log.d("LOLnum", "lol");
-                numOfTimesDisplayed = (stopTimeDisplayed - startTimeDisplayed) / gap +1;
+                numOfTimesDisplayed = (stopTimeDisplayed - startTimeDisplayed) / gap + 1;
                 // podać liczbę godzin, które się wyświetlają (normalnie jest zawsze 17)
             }
         }
 
-    }
-
-
-    private void addRectangleClass(int startTime, int stopTime, int dayOfWeek) { // jeszcze jakiś text w środku i kolor rectangla i textu
-        // zakładam, że zawsze bedzie 17 czasów pokazanych
-        // zakładam, że podają co min 30 min (potem sie zmieni)
-        // dstac y pierwszej lini i ostatniej a potem dzielic biorąc czas pomiędzy wszystkimi a czas trwania lekcji
-        int numOfTimesDisplayed = 16;
-        int i = 0;  // 8:00
-        int y1 = DAYS_SECTION_SIZE + (i * 2 + 1) * ((scheduleHeight / ((numOfTimesDisplayed + 1) * 2)));
-        int j = numOfTimesDisplayed; // 16:00
-        int y2 = DAYS_SECTION_SIZE + (j * 2 + 1) * ((scheduleHeight / ((numOfTimesDisplayed + 1) * 2)));
-
-        int spaceBetweenStartAndEnd = y2 - y1; // ogólna wysokosc od 8 do 16
-        // dodawac y1 bo od tego ma iść
-        // TODO
     }
 
     private void setCanvas(SurfaceHolder holder) {
@@ -314,15 +334,6 @@ public class TimetableCanvas extends AppCompatActivity implements SurfaceHolder.
         paint.setColor(Color.WHITE);
         canvas.drawPaint(paint);
 
-    }
-
-    private void makeRectange(int left, int top, int right, int bottom, String hexColor) {
-        Rect rect = new Rect(left, top, right, bottom);
-        rectangleClasses.add(rect);
-        Paint paint = new Paint();
-        paint.setStyle(Paint.Style.FILL);
-        paint.setColor(Color.parseColor(hexColor));
-        canvas.drawRect(rect, paint);
     }
 
     private void drawText(String text) {
@@ -336,8 +347,6 @@ public class TimetableCanvas extends AppCompatActivity implements SurfaceHolder.
         // upierdalać text jak wychodzi poza obszar
         // wpisywac na środku
     }
-
-    // funkcja do robienia rectangla z textem
 
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
