0f7e9fee74e785e07bfc45b1a6c3bbf7f29b6a68
jakub <qbabor4444@gmail.com>
2018-01-08 20:06:01 +0100

    working on opening aplication when clicking on widget

--- app/src/main/java/qbabor4/pl/schoolschedule/NextClassWidget.java
+++ app/src/main/java/qbabor4/pl/schoolschedule/NextClassWidget.java
@@ -3,9 +3,11 @@ package qbabor4.pl.schoolschedule;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ActivityNotFoundException;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.util.Log;
 import android.widget.RemoteViews;
@@ -21,12 +23,12 @@ import java.util.HashMap;
  * podmienić textView na godziny
  * metoda dostajaca klasę i zmieniająca dane klasy na widgecie
  * Ma ustawiać alarm w momencie rozpoczęcia kolejnych zajęć a nie zakończenia tych co są
+ * Jak jest widget na ekarnie i sie odpala jeszcze raz aplikację, to wywala błąd
  *
  */
 public class NextClassWidget extends AppWidgetProvider {
 
-    public static String YOUR_AWESOME_ACTION = "YourAwesomeAction";
-
+    public static String OPEN_APP_ACTION = "qbabor4.pl.schoolschedule.OPEN_APP";
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -38,27 +40,35 @@ public class NextClassWidget extends AppWidgetProvider {
         Log.d("lol4", intent.getAction() + "out");
 
         if (intent != null) {
-            if (intent.getAction().equals(Intent.ACTION_ANSWER)) {
-                Log.d("lol4", intent.getAction());
-
+            /** When got intent from alarm or when new widget is added */ // moze to rozbic i jak bedzie dodawany, to bedzie usuwany alarm i tworzony nowy?
+            if (intent.getAction().equals(Intent.ACTION_ANSWER) || intent.getAction().equals("android.appwidget.action.APPWIDGET_UPDATE")) {
                 Alarm alarm = new Alarm();
                 Cursor cursor = alarm.getNextSubjectData();
 
-
                 Toast.makeText(context, intent.getAction() + "lol3", Toast.LENGTH_LONG).show();
                 updateAllWidgets(context, AppWidgetManager.getInstance(context), AppWidgetManager.getInstance(context).getAppWidgetIds(new ComponentName(context, NextClassWidget.class)), alarm.getDataFromCursor(cursor));
-//                onUpdate(context, AppWidgetManager.getInstance(context), AppWidgetManager.getInstance(context).getAppWidgetIds(new ComponentName(context, NextClassWidget.class)));
-                // zobaczyc czy to na gorze działa (chyba dziala
-                // przekazać mapę do zmiany danych do onUpdate
-                // jak działą, to dać updatowanie do updateAppWidget
-                // nie uzywac updatwowania
-            }
-            else if(intent.getAction().equals("android.appwidget.action.APPWIDGET_UPDATE")){
-                Log.d("lol4", "OK");
-                // ustawić onClick
+
+                // ustawić koleny alarm
+            } else if(intent.getAction().equals(OPEN_APP_ACTION)){
+                openApp(context, "qbabor4.pl.schoolschedule"); // nie działa
             }
         }
+    }
 
+    public static boolean openApp(Context context, String packageName) {
+        PackageManager manager = context.getPackageManager();
+        try {
+            Intent i = manager.getLaunchIntentForPackage(packageName);
+            if (i == null) {
+                return false;
+                //throw new ActivityNotFoundException();
+            }
+            i.addCategory(Intent.CATEGORY_LAUNCHER);
+            context.startActivity(i);
+            return true;
+        } catch (ActivityNotFoundException e) {
+            return false;
+        }
     }
 
     private void updateAllWidgets(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds, HashMap<SqlDataEnum, String> classData){
@@ -79,10 +89,18 @@ public class NextClassWidget extends AppWidgetProvider {
         views.setTextViewText(R.id.tv_subject, classData.get(SqlDataEnum.SUBJECT));
         views.setTextViewText(R.id.tv_classroom, classData.get(SqlDataEnum.CLASSROOM));
 
+        createWidgetOnClickListener(context, views);
         // jak puste pola, to napisać, że brak
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    private void createWidgetOnClickListener(Context context, RemoteViews views){
+        Intent openAppIntent = new Intent(context, this.getClass());
+        openAppIntent.setAction(OPEN_APP_ACTION);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, openAppIntent, 0);
+        views.setOnClickPendingIntent(R.id.widget_linearLayout, pendingIntent);
     }
 
     @Override
